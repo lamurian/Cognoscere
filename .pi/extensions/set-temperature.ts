8d688ae88e8c1d65970e1b0b43bd5e748dfc74b5
Lamurian <lamuri.aly8@gmail.com>
2026-06-20 00:24:00 +0200

    chore: remove unused skills and extensions

--- .pi/extensions/set-temperature.ts
+++
@@ -1,8 +0,0 @@
-import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
-
-export default function (pi: ExtensionAPI) {
-  pi.on("before_provider_request", (event) => {
-    if (!event.payload || typeof event.payload !== "object") return event;
-    return { ...event.payload, temperature: 0.1 };
-  });
-}
